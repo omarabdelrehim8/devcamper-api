21dfaa371d9f29519f7a19d6537d33edf7250c6f
Omar Abdel Rehim <136030333+omarabdelrehim8@users.noreply.github.com>
2023-11-07 05:18:16 +0100

    added courses crud functionality and aggregate for avg cost

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const customError = require("../utils/customError");
 const asyncErrorHandler = require("../middleware/asyncErrorHandler");
 const Course = require("../models/Course");
+const Bootcamp = require("../models/Bootcamp");
 
 // @desc       Get all courses / Get all courses of a specific bootcamp
 // @route      GET /api/v1/courses
@@ -25,4 +26,80 @@ exports.getCourses = asyncErrorHandler(async (req, res, next) => {
   const courses = await query;
 
   res.status(200).send({ success: true, count: courses.length, data: courses });
+});
+
+// @desc       Get single courses
+// @route      GET /api/v1/courses/:id
+// @access     Public
+exports.getCourse = asyncErrorHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name description",
+  });
+
+  if (!course) {
+    return next(
+      new customError(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+  res.status(200).send({ success: true, data: course });
+});
+
+// @desc       Add course
+// @route      POST /api/v1/bootcamps/:bootcampId/courses (We do this because every course is tied to a bootcamp so we have to go through the bootcamp ID to add a new course, basically we are adding a new course to a bootcamp)
+// @access     Private (adding a new course is a private operation that only a logged in user should be able to do)
+exports.addCourse = asyncErrorHandler(async (req, res, next) => {
+  // In the req body we will get all the course fields we want to include in the new course. These fields are inputted by the user and are submitted to the Course model. Some fields though have to be added manually by us developers, like the "bootcamp" field in the Course model, which ties the course to a bootcamp through its ID.
+  // The line of code below is adding the bootcampId that we are getting from the req to the "bootcamp" field in the Course model
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new customError(`No bootcamp with the id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  const course = await Course.create(req.body);
+  res.status(200).send({ success: true, data: course });
+});
+
+// @desc       Update course
+// @route      PUT /api/v1/courses/:id
+// @access     Private
+exports.updateCourse = asyncErrorHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new customError(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).send({ success: true, data: course });
+});
+
+// @desc       Delete course
+// @route      DELETE /api/v1/courses/:id
+// @access     Private
+exports.deleteCourse = asyncErrorHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new customError(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  await course.deleteOne();
+  res.status(200).send({ success: true, data: {} });
 });
--- models/Course.js
+++ models/Course.js
@@ -39,4 +39,47 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+// Static method (that we call directly on the model) to get average cost of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log(`Calculating avg cost...`);
+
+  // aggregate is a mongoDB feature which allows us to aggregate data, for example, calculating averages, min and max values, sum etc., mongoose gives us access to the aggregate framework and feature through the aggregate() function
+  // the method aggregate() return a promise, that's why we use await. Since we are already inside of the model we use the "this" keyword that, in this case,points to the model itself. When using "this", we get a reference to the current model. If we'd like to switch to a different model, we could do this.model('Bootcamp') to move our reference to the Bootcamp model.
+  const arr = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    // we will get an object with this fields
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  // Put those fields in the database
+  // We are grabbing our bootcamp model, updating that specific bootcamp by the ID and we are adding the result of the aggregation, the averageCost into the averageCost field
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost:
+        arr.length > 0 ? Math.ceil(arr[0].averageCost / 10) * 10 : null,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call the method getAverageCost after save, because if we add a course or remove it we want re-calculate the average cost
+CourseSchema.post("save", async function () {
+  // "this" keyword point to the model.
+  // On save, we save the id of the bootcamp inside the bootcamp field sothis.bootcamp points to that id.
+  await this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call the method getAverageCost after remove
+CourseSchema.post("deleteOne", { document: true }, async function () {
+  await this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
